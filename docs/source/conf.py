504c171f510941ea8f4acec978d8c9ee41d52907
Patrick Wang <wangyunze16@gmail.com>
2024-11-22 09:30:01 +0000

    Document framework added

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+import dedi_link
+from packaging.version import Version
+
+
+project = 'Decentralised Discovery Link'
+copyright = '2024, Firefox2100'
+author = 'Firefox2100'
+
+# Extract version from package
+_v = Version(dedi_link.__version__)
+
+release = str(_v)
+version = f'{_v.major}.{_v.minor}'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ docs/source/index.rst
@@ -0,0 +1,15 @@
+Decentralised Discovery Link
+===================================
+
+Decentralised Discovery Link is a project that implements a decentralised protocol designed specifically for data discovery in healthcare. It aims to provide developers and software a way to quickly setup such protocol, integrate with existing systems, and experiment on new ideas.
+
+.. note::
+
+    This project is under active development. API may change without prior notice. Do NOT upgrade without reading the release notes first, especially between major versions.
+
+Contents
+--------
+
+.. toctree::
+
+    about
